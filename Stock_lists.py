b119f2edb071a949123258bc2444eb5ca45b0560
Con <gmunster33@gmail.com>
2024-03-09 17:54:14 -0500

    Added separate file to store lists of stocks for reference

---
+++ Stock_lists.py
@@ -0,0 +1,87 @@
+
+# This list has more mainstream, large-cap, and well-known stocks, including the 30 stocks in the Dow Jones Industrial Average (DJIA) and the 500 stocks in the S&P 500 Index. This list is more suitable for general stock market analysis and investing.
+extended_tickers = [
+    "INTC",  # Intel Corporation
+    "QCOM",  # Qualcomm Incorporated
+    "AVGO",  # Broadcom Inc.
+    "NVDA",  # NVIDIA Corporation
+    "AMD",   # Advanced Micro Devices, Inc.
+    "MU",    # Micron Technology, Inc.
+    "TXN",   # Texas Instruments Incorporated
+    "AMAT",  # Applied Materials, Inc.
+    "ASML",  # ASML Holding N.V.
+    "LRCX",  # Lam Research Corporation
+    "SONY",  # Sony Group Corporation
+    "GLW",   # Corning Incorporated
+    "TSM",   # Taiwan Semiconductor Manufacturing Company
+    "WDC",   # Western Digital Corporation
+    "STX",   # Seagate Technology Holdings plc
+    "KEYS",  # Keysight Technologies
+    "KLAC",  # KLA Corporation
+    "GOOG",  # Alphabet Inc. (Google)
+    "MSFT",  # Microsoft Corporation
+    "META",  # Meta Platforms, Inc. (formerly Facebook)
+    "DELL",  # Dell Technologies Inc.
+    "HPQ",   # HP Inc.
+    "ADBE",  # Adobe Inc.
+    "CRM",   # Salesforce
+    "ORCL",  # Oracle Corporation
+    "IBM",   # International Business Machines Corporation
+    "CSCO",  # Cisco Systems, Inc.
+    "SAP",   # SAP SE
+    "INTU",  # Intuit Inc.
+    "SQ",    # Block, Inc. (formerly Square, Inc.)
+    "SHOP",  # Shopify Inc.
+    "SNAP",  # Snap Inc.
+    "TSLA",  # Tesla, Inc.
+    "PYPL",  # PayPal Holdings, Inc.
+    "ADSK",  # Autodesk, Inc.
+    "ANSS",  # ANSYS, Inc.
+    "CTSH",  # Cognizant Technology Solutions Corporation
+    "INFY",  # Infosys Limited
+    "ERIC",  # Telefonaktiebolaget LM Ericsson (publ)
+    "NOK",   # Nokia Corporation
+    "V",     # Visa Inc.
+    "MA",    # Mastercard Incorporated
+    "AMZN",  # Amazon.com, Inc.
+    "ZM",    # Zoom Video Communications, Inc.
+    "UBER",  # Uber Technologies, Inc.
+    "LYFT",  # Lyft, Inc.
+    # Additional 40 similar American tech stocks
+    "EA",    # Electronic Arts Inc.
+    "TTWO",  # Take-Two Interactive Software, Inc.
+    "NTDOY", # Nintendo Co., Ltd.
+    "ROKU",  # Roku, Inc.
+    "NFLX",  # Netflix, Inc.
+    "DIS",   # The Walt Disney Company
+    "TWLO",  # Twilio Inc.
+    "OKTA",  # Okta, Inc.
+    "DDOG",  # Datadog, Inc.
+    "ZS",    # Zscaler, Inc.
+    "PANW",  # Palo Alto Networks, Inc.
+    "FTNT",  # Fortinet, Inc.
+    "CHKP",  # Check Point Software Technologies Ltd.
+    "SPLK",  # Splunk Inc.
+    "WDAY",  # Workday, Inc.
+    "NOW",   # ServiceNow, Inc.
+    # "CTXS",  # Citrix Systems, Inc.
+    # "DOCU",  # DocuSign, Inc.
+    # "CRWD",  # CrowdStrike Holdings, Inc.
+    # "OKTA",  # Okta, Inc.
+    # "FSLY",  # Fastly, Inc.
+    # "NET",   # Cloudflare, Inc.
+    # "SNOW",  # Snowflake Inc.
+    # "MDB",   # MongoDB, Inc.
+    # "PLTR",  # Palantir Technologies Inc.
+    # "GME",   # GameStop Corp.
+    # "SPOT",  # Spotify Technology S.A.
+    # "SQ",    # Square, Inc. (renamed to Block, Inc., included twice for emphasis)
+    # "TEAM",  # Atlassian Corporation Plc
+    # "COUP",  # Coupa Software Incorporated
+    # "VEEV",  # Veeva Systems Inc.
+    # "AYX",   # Alteryx, Inc.
+    # "SMAR",  # Smartsheet Inc.
+    # "WORK",  # Slack Technologies, Inc. (Note: Acquired by Salesforce)
+    # "ZM",    # Zoom Video Communications, Inc. (included twice for emphasis)
+    # "BOX",   # Box, Inc.
+]
